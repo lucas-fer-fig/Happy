3dcd570408cca03034c116403b850245f836689d
lucas-fer-fig <figueiredo.lucasfernandes@gmail.com>
2020-10-20 21:56:52 -0400

    Conclusão do workshop 3

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import CreateOrphanage from "./pages/CreateOrphanage";
 import Landing from "./pages/Landing";
-import OrphanagesMap from "./pages/OrphanagesMapMap";
+import Orphanage from "./pages/Orphanage";
+import OrphanagesMap from "./pages/OrphanagesMap";
 
 function Routes() {
   return (
@@ -9,6 +11,8 @@ function Routes() {
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/app" component={OrphanagesMap} />
+        <Route exact path="/orphanages/create" component={CreateOrphanage} />
+        <Route exact path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
   );
